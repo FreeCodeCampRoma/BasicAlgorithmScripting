2ee54a36008c742ab8e25117b4cb32df900b3c8c
LordKenzo <lordkenzo@outlook.com>
2017-01-04 10:59:23 +0100

    Commento in test di Reverse String

--- Test/reverseString-test.js
+++ Test/reverseString-test.js
@@ -1,6 +1,6 @@
 // Importo, tramite sintassi CommonJS, il modulo expect di CHAI per l'esecuzione del test.
 var expect = require('chai').expect
-// Importo tutti gli script esportati dal file index.js in ../Mutations
+// Importo tutti gli script esportati dal file index.js in ../Reverse String
 var reverseStringLibrary = require('../Reverse String');
 
 describe('Reverse String Library Scripts', function() {
