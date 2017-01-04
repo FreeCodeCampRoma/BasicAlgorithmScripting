bdcc257cee3c917875e2179fb1b2080d5df050fc
LordKenzo <lordkenzo@outlook.com>
2017-01-04 10:51:37 +0100

    Aggiunto commento in mutations_variante02.js

--- Mutations/mutations_variante02.js
+++ Mutations/mutations_variante02.js
@@ -1,3 +1,4 @@
+// La every controlla che tutti gli elementi rispettino la condizione
 module.exports = function mutation(arr){
   return arr[1].split('').every(function(carattere){
   	return arr[0].indexOf(carattere) !== -1;
