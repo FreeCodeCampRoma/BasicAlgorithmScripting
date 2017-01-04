4f0cfa12850b22a5b33d403000a456ead5b28664
Lorenzo Franceschini <postaforum@gmail.com>
2017-01-04 10:45:16 +0100

    Inserimento commento in mutations_variante01

--- Mutations/mutations_variante01.js
+++ Mutations/mutations_variante01.js
@@ -1,3 +1,4 @@
+// Con la some vogliamo tornare il negato perchè la some ritorna true se un elemento non è presente. Per cui la logica della funzione mutation è di tornare true se tutte le occorrenze delle lettere sono presenti. Per questo motivo cambierò il true di some in false, se ho caratteri non presenti!
 module.exports = function mutation(arr){
   return !arr[1].split('').some(function(carattere){
   	return arr[0].indexOf(carattere) == -1;
